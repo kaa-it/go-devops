42d366bff3e8a0e4bb2809395bad04b122d19fc0
Andrey Kruglov <kruglov.it@gmail.com>
2024-05-08 19:58:09 +0300

    feat: thirteenth iteration complete

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/go-resty/resty/v2"
 	"github.com/joho/godotenv"
 	"github.com/kaa-it/go-devops/internal/agent"
+	"time"
 )
 
 func main() {
@@ -11,6 +12,13 @@ func main() {
 
 	config := agent.NewConfig()
 	client := resty.New()
+
+	client.SetRetryCount(3)
+	client.SetRetryWaitTime(time.Second)
+	client.SetRetryMaxWaitTime(5 * time.Second)
+	client.SetRetryAfter(func(client *resty.Client, resp *resty.Response) (time.Duration, error) {
+		return 2 * time.Second, nil
+	})
 
 	metricsAgent := agent.New(client, config)
 
